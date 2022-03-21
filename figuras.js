f7e796acf92908b5c0e060e1268966e1baefd23c
raifels <raifelsalazars@gmail.com>
2022-03-21 05:00:39 -0500

    última edicion del lunes 21 mar

--- figuras.js
+++ figuras.js
@@ -1,12 +1,24 @@
 // CÓDIGO DEL CUADRADO
 const answerCua = Number(prompt("Cuanto mide un lado de tu cudrado"));
-document.write(`El area de tu cuadrado es ${answerCua * answerCua}cm cudrados 
-Y su perimetro es ${answerCua * 4}cm`)
+
+var answerCuaArea = answerCua * answerCua;
+var answerCuaPeri = answerCua * 4;
+
+document.write("El area de tu cuadrado es " + 
+    answerCuaArea + 
+    " cm²" + 
+    ".\nY su perimetro es " + 
+    answerCuaPeri + 
+    " cm");
+
 // CÓDIGO DEL TRIÁNGULO
-const datosTriangulo =(prompt("Ingresa la altura de tu triangulo y luego de un espacio la base?"));
+const datosTriangulo = prompt("Ingresa la altura de tu triangulo y luego de un espacio la base?");
 let arrayDatosTriangulo = datosTriangulo.split(" ");
+
 for (let dato of arrayDatosTriangulo) {
 arrayDatosTriangulo.push(Number(arrayDatosTriangulo.shift()));
 }
-document.write(`
- El area de tu triángulo es ${(arrayDatosTriangulo[1] * arrayDatosTriangulo[0]) / 2}cm`)
+
+var areaTriangulo = (arrayDatosTriangulo[1] * arrayDatosTriangulo[0]) / 2;
+
+document.write(".\nEl area de tu triángulo es " + areaTriangulo + " cm²")
